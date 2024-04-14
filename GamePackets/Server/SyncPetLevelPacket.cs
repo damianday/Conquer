cec2edacbba112821c1cd454c6c716b5d3220ec3
Damian Day <damianday@users.noreply.github.com>
2024-04-14 21:31:01 +0100

    SyncPetLevelPacket

--- GamePackets/Server/SyncPetLevelPacket.cs
+++ GamePackets/Server/SyncPetLevelPacket.cs
@@ -1,7 +1,7 @@
 namespace GamePackets.Server;
 
 [PacketInfo(Source = PacketSource.Server, ID = 72, Length = 7, Description = "同步宠物等级", Broadcast = true)]
-public sealed class 同步宠物等级 : GamePacket
+public sealed class SyncPetLevelPacket : GamePacket
 {
     [FieldAttribute(Position = 2, Length = 4)]
     public int ObjectID;
