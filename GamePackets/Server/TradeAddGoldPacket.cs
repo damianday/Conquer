2ecdb4a6eecedb7c2b0c3e7f6f3861e6388273fb
Damian Day <damianday@users.noreply.github.com>
2024-04-16 17:44:06 +0100

    TradeAddGoldPacket

--- GamePackets/Server/TradeAddGoldPacket.cs
+++ GamePackets/Server/TradeAddGoldPacket.cs
@@ -1,7 +1,7 @@
 namespace GamePackets.Server;
 
 [PacketInfo(Source = PacketSource.Server, ID = 217, Length = 10, Description = "放入交易金币")]
-public sealed class 放入交易金币 : GamePacket
+public sealed class TradeAddGoldPacket : GamePacket
 {
     [FieldAttribute(Position = 2, Length = 4)]
     public int 对象编号;
