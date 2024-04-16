92dc5e6c7db50fbbd645890d4c2dbfeda2dc1939
Damian Day <damianday@users.noreply.github.com>
2024-04-16 17:43:57 +0100

    TradeAddItemPacket

--- GamePackets/Server/TradeAddItemPacket.cs
+++ GamePackets/Server/TradeAddItemPacket.cs
@@ -1,17 +1,17 @@
 namespace GamePackets.Server;
 
 [PacketInfo(Source = PacketSource.Server, ID = 216, Length = 0, Description = "放入交易物品")]
-public sealed class 放入交易物品 : GamePacket
+public sealed class TradeAddItemPacket : GamePacket
 {
     [FieldAttribute(Position = 4, Length = 4)]
-    public int 对象编号;
+    public int ObjectID;
 
     [FieldAttribute(Position = 8, Length = 1)]
-    public byte 放入位置;
+    public byte Location;
 
     [FieldAttribute(Position = 9, Length = 1)]
     public byte 放入物品;
 
     [FieldAttribute(Position = 10, Length = 0)]
-    public byte[] 物品描述;
+    public byte[] Description;
 }
