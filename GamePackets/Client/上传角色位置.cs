db241426ee29d5e8cbf691367981333e4dfac1e0
Damian Day <damianday@users.noreply.github.com>
2024-04-17 12:15:10 +0100

    Update 上传角色位置.cs

--- GamePackets/Client/上传角色位置.cs
+++ GamePackets/Client/上传角色位置.cs
@@ -6,8 +6,8 @@ namespace GamePackets.Client;
 public sealed class 上传角色位置 : GamePacket
 {
     [FieldAttribute(Position = 4, Length = 4)]
-    public Point 坐标;
+    public Point Location;
 
     [FieldAttribute(Position = 8, Length = 2)]
-    public ushort 高度;
+    public ushort Height;
 }
