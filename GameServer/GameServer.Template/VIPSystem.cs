195aa4f391291c71d5b2818e61d138cecc87a686
Damian Day <damianday@users.noreply.github.com>
2024-04-25 12:44:29 +0100

    Update VIPSystem.cs

--- GameServer/GameServer.Template/VIPSystem.cs
+++ GameServer/GameServer.Template/VIPSystem.cs
@@ -1,5 +1,9 @@
+using CsvHelper;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
 
 namespace GameServer.Template;
 
@@ -23,43 +27,83 @@ public sealed class VIPSystem
     {
         DataSheet = new Dictionary<int, VIPSystem>();
 
-        if (!DBAgent.X.Connected)
-            return;
-
-        try
+        if (Config.DBMethod == 1)
         {
-            var qstr = "SELECT * FROM VIPSystem";
-            using (var connection = DBAgent.X.DB.GetConnection())
-            {
-                using var command = DBAgent.X.DB.GetCommand(connection, qstr);
+            if (!DBAgent.X.Connected)
+                return;
 
-                using var reader = command.ExecuteReader();
-                if (reader != null)
+            try
+            {
+                var qstr = "SELECT * FROM VIPSystem";
+                using (var connection = DBAgent.X.DB.GetConnection())
                 {
-                    while (reader.Read() == true)
+                    using var command = DBAgent.X.DB.GetCommand(connection, qstr);
+
+                    using var reader = command.ExecuteReader();
+                    if (reader != null)
                     {
-                        var obj = new VIPSystem
+                        while (reader.Read() == true)
                         {
-                            VIPID = reader.GetInt32("VIP编号"),
-                            NeedVIPPoints = reader.GetInt32("VIP所需积分"),
-                            VIPIngot = reader.GetInt32("VIP元宝受益"),
-                            VIPGoldCoin = reader.GetInt32("VIP金币受益"),
-                            装备宝箱编号 = reader.GetInt32("装备宝箱编号"),
-                            材料宝箱编号 = reader.GetInt32("材料宝箱编号"),
-                            装备宝箱数量 = reader.GetInt32("装备宝箱数量"),
-                            材料宝箱数量 = reader.GetInt32("材料宝箱数量"),
-                            VIPExperience = reader.GetInt32("VIP经验收益"),
-                            VIP收益间隔 = reader.GetInt32("VIP收益间隔"),
-                            获得收益等级 = reader.GetInt32("获得收益等级")
-                        };
-                        DataSheet.Add(obj.VIPID, obj);
+                            var obj = new VIPSystem
+                            {
+                                VIPID = reader.GetInt32("VIP编号"),
+                                NeedVIPPoints = reader.GetInt32("VIP所需积分"),
+                                VIPIngot = reader.GetInt32("VIP元宝受益"),
+                                VIPGoldCoin = reader.GetInt32("VIP金币受益"),
+                                装备宝箱编号 = reader.GetInt32("装备宝箱编号"),
+                                材料宝箱编号 = reader.GetInt32("材料宝箱编号"),
+                                装备宝箱数量 = reader.GetInt32("装备宝箱数量"),
+                                材料宝箱数量 = reader.GetInt32("材料宝箱数量"),
+                                VIPExperience = reader.GetInt32("VIP经验收益"),
+                                VIP收益间隔 = reader.GetInt32("VIP收益间隔"),
+                                获得收益等级 = reader.GetInt32("获得收益等级")
+                            };
+                            DataSheet.Add(obj.VIPID, obj);
+                        }
                     }
                 }
             }
+            catch (Exception err)
+            {
+                SMain.AddSystemLog(err.ToString());
+            }
         }
-        catch (Exception err)
+
+        if (Config.DBMethod == 2)
         {
-            SMain.AddSystemLog(err.ToString());
+            var path = Config.GameDataPath + "\\System\\VIPSystem";
+            if (!Directory.Exists(path))
+                return;
+
+            using var reader = new StreamReader(path + "\\VIP系统数据.csv", Encoding.GetEncoding("GB18030"));
+            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csvReader.Read();
+            csvReader.ReadHeader();
+            try
+            {
+                while (csvReader.Read())
+                {
+                    var obj = new VIPSystem
+                    {
+                        VIPID = csvReader.GetField<int>("VIP编号"),
+                        NeedVIPPoints = csvReader.GetField<int>("VIP所需积分"),
+                        VIPIngot = csvReader.GetField<int>("VIP元宝受益"),
+                        VIPGoldCoin = csvReader.GetField<int>("VIP金币受益"),
+                        装备宝箱编号 = csvReader.GetField<int>("装备宝箱编号"),
+                        材料宝箱编号 = csvReader.GetField<int>("材料宝箱编号"),
+                        装备宝箱数量 = csvReader.GetField<int>("装备宝箱数量"),
+                        材料宝箱数量 = csvReader.GetField<int>("材料宝箱数量"),
+                        VIPExperience = csvReader.GetField<int>("VIP经验收益"),
+                        VIP收益间隔 = csvReader.GetField<int>("VIP收益间隔"),
+                        获得收益等级 = csvReader.GetField<int>("获得收益等级")
+                    };
+                    DataSheet.Add(obj.VIPID, obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                SEngine.AddSystemLog(ex.Message);
+            }
         }
     }
 }
