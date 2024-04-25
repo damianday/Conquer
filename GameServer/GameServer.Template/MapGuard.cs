4409e620a48a304e4140703d2afd8f316b31c131
Damian Day <damianday@users.noreply.github.com>
2024-04-25 12:43:57 +0100

    Update MapGuard.cs

--- GameServer/GameServer.Template/MapGuard.cs
+++ GameServer/GameServer.Template/MapGuard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 
 namespace GameServer.Template;
 
@@ -19,39 +20,53 @@ public sealed class MapGuard
     {
         DataSheet = new HashSet<MapGuard>();
 
-        if (!DBAgent.X.Connected)
-            return;
-
-        try
+        if (Config.DBMethod == 0)
         {
-            var qstr = "SELECT * FROM GuardZen";
-            using (var connection = DBAgent.X.DB.GetConnection())
+            var path = Config.GameDataPath + "\\System\\GameMap\\Guards\\";
+            if (!Directory.Exists(path))
+                return;
+
+            var array = Serializer.Deserialize<MapGuard>(path);
+            foreach (var obj in array)
+                DataSheet.Add(obj);
+        }
+
+        if (Config.DBMethod == 1)
+        {
+            if (!DBAgent.X.Connected)
+                return;
+
+            try
             {
-                using var command = DBAgent.X.DB.GetCommand(connection, qstr);
-
-                using var reader = command.ExecuteReader();
-                if (reader != null)
+                var qstr = "SELECT * FROM GuardZen";
+                using (var connection = DBAgent.X.DB.GetConnection())
                 {
-                    while (reader.Read() == true)
-                    {
-                        var guard = new MapGuard();
-                        guard.GuardID = reader.GetUInt16("GuardID");
-                        guard.MapID = reader.GetByte("MapID");
-                        guard.MapName = reader.GetString("MapName");
-                        guard.Coordinates.X = reader.GetInt32("MapPosX");
-                        guard.Coordinates.Y = reader.GetInt32("MapPosY");
-                        guard.Direction = (GameDirection)reader.GetUInt16("Direction");
-                        guard.RegionName = reader.GetString("RegionName");
+                    using var command = DBAgent.X.DB.GetCommand(connection, qstr);
 
-                        DataSheet.Add(guard);
+                    using var reader = command.ExecuteReader();
+                    if (reader != null)
+                    {
+                        while (reader.Read() == true)
+                        {
+                            var guard = new MapGuard();
+                            guard.GuardID = reader.GetUInt16("GuardID");
+                            guard.MapID = reader.GetByte("MapID");
+                            guard.MapName = reader.GetString("MapName");
+                            guard.Coordinates.X = reader.GetInt32("MapPosX");
+                            guard.Coordinates.Y = reader.GetInt32("MapPosY");
+                            guard.Direction = (GameDirection)reader.GetUInt16("Direction");
+                            guard.RegionName = reader.GetString("RegionName");
+
+                            DataSheet.Add(guard);
+                        }
                     }
                 }
             }
-        }
-        catch (Exception err)
-        {
-            SMain.AddSystemLog(err.ToString());
-            return;
+            catch (Exception err)
+            {
+                SMain.AddSystemLog(err.ToString());
+                return;
+            }
         }
     }
 }
