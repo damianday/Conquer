c5119239f1a52b9a18ac7a515c3e260aa1fc36a5
Damian Day <damianday@users.noreply.github.com>
2024-04-25 15:38:16 +0100

    Update MapArea.cs

--- GameServer/GameServer.Template/MapArea.cs
+++ GameServer/GameServer.Template/MapArea.cs
@@ -1,7 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 
 namespace GameServer.Template;
 
@@ -11,36 +11,62 @@ public class MapArea
 
     public byte MapID;
     public string MapName;
-    public Point Coordinates;
     public string RegionName;
+    public Point Coordinates = Point.Empty;
     public int AreaRadius;
     public AreaType RegionType;
-    public HashSet<Point> RangeCoordinates;
-
-    private List<Point> m_ListCoords;
-
-    public Point RandomCoords => RangeCoordsList[SEngine.Random.Next(RangeCoordsList.Count)];
-
-    public List<Point> RangeCoordsList
-    {
-        get
-        {
-            if (m_ListCoords == null)
-                m_ListCoords = RangeCoordinates.ToList();
-            return m_ListCoords;
-        }
-    }
 
     public static void LoadData()
     {
         DataSheet = new List<MapArea>();
 
-        var path = Config.GameDataPath + "\\System\\GameMap\\MapAreas\\";
-        if (!Directory.Exists(path))
-            return;
+        if (Config.DBMethod == 0)
+        {
+            var path = Config.GameDataPath + "\\System\\GameMap\\MapAreas\\";
+            if (!Directory.Exists(path))
+                return;
 
-        var array = Serializer.Deserialize<MapArea>(path);
-        foreach (var obj in array)
-            DataSheet.Add(obj);
+            var array = Serializer.Deserialize<MapArea>(path);
+            foreach (var obj in array)
+                DataSheet.Add(obj);
+        }
+
+        if (Config.DBMethod == 1)
+        {
+            if (!DBAgent.X.Connected)
+                return;
+
+            try
+            {
+                var qstr = "SELECT * FROM MapRegions";
+                using (var connection = DBAgent.X.DB.GetConnection())
+                {
+                    using var command = DBAgent.X.DB.GetCommand(connection, qstr);
+
+                    using var reader = command.ExecuteReader();
+                    if (reader != null)
+                    {
+                        while (reader.Read() == true)
+                        {
+                            var region = new MapArea();
+                            region.MapID = reader.GetByte("MapID");
+                            region.MapName = reader.GetString("MapName");
+                            region.RegionName = reader.GetString("RegionName");
+                            region.Coordinates.X = reader.GetInt32("MapPosX");
+                            region.Coordinates.Y = reader.GetInt32("MapPosY");
+                            region.AreaRadius = reader.GetInt32("Radius");
+                            region.RegionType = Enum.Parse<AreaType>(reader.GetString("RegionType"));
+
+                            DataSheet.Add(region);
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                SMain.AddSystemLog(err.ToString());
+                return;
+            }
+        }
     }
 }
