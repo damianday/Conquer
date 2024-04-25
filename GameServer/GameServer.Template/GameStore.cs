9b522c5b0d42c528c65498b1201ae2086e1b71e5
Damian Day <damianday@users.noreply.github.com>
2024-04-25 19:46:39 +0100

    Update GameStore.cs

--- GameServer/GameServer.Template/GameStore.cs
+++ GameServer/GameServer.Template/GameStore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,26 +26,96 @@ public sealed class GameStore
 
     public int StoreID;
     public string Name;
-    public SaleType StoreType;
-    public List<GameStoreItem> Products;
+    public SaleType StoreType = SaleType.All;
+    public List<GameStoreItem> Products = new List<GameStoreItem>();
     public SortedSet<ItemInfo> AvailableItems = new SortedSet<ItemInfo>(new PurchaseComparer());
 
     public static void LoadData()
     {
         DataSheet = new Dictionary<int, GameStore>();
 
-        var path = Config.GameDataPath + "\\System\\Items\\GameStore\\"; // "\\System\\物品数据\\游戏商店\\"
-        if (Directory.Exists(path))
+        if (Config.DBMethod == 0)
         {
+            var path = Config.GameDataPath + "\\System\\Items\\GameStore\\"; // "\\System\\物品数据\\游戏商店\\"
+            if (!Directory.Exists(path))
+                return;
+
             var array = Serializer.Deserialize<GameStore>(path);
             foreach (var obj in array)
                 DataSheet.Add(obj.StoreID, obj);
         }
 
+        if (Config.DBMethod == 1)
+        {
+            if (!DBAgent.X.Connected)
+                return;
+
+            try
+            {
+                var qstr = "SELECT * FROM GameStores";
+                using (var connection = DBAgent.X.DB.GetConnection())
+                {
+                    using var command = DBAgent.X.DB.GetCommand(connection, qstr);
+
+                    using var reader = command.ExecuteReader();
+                    if (reader != null)
+                    {
+                        while (reader.Read() == true)
+                        {
+                            var store = new GameStore();
+                            store.StoreID = reader.GetInt32("StoreID");
+                            store.Name = reader.GetString("Name");
+                            store.StoreType = (SaleType)reader.GetInt32("StoreType");
+                            
+                            DataSheet.Add(store.StoreID, store);
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                SMain.AddSystemLog(err.ToString());
+                return;
+            }
+
+            try
+            {
+                foreach (var store in DataSheet)
+                {
+                    var qstr = "SELECT * FROM GameStoreProduct WHERE StoreID=@StoreID";
+                    using (var connection = DBAgent.X.DB.GetConnection())
+                    {
+                        using var command = DBAgent.X.DB.GetCommand(connection, qstr);
+                        command.Parameters.AddWithValue("@StoreID", store.Value.StoreID);
+
+                        using var reader = command.ExecuteReader();
+                        if (reader != null)
+                        {
+                            while (reader.Read() == true)
+                            {
+                                var product = new GameStoreItem();
+                                product.ItemID = reader.GetInt32("ItemID");
+                                product.Quantity = reader.GetInt32("Quantity");
+                                product.CurrencyModel = reader.GetInt32("CurrencyModel");
+                                product.Price = reader.GetInt32("Price");
+
+                                store.Value.Products.Add(product);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                SMain.AddSystemLog(err.ToString());
+                return;
+            }
+        }
+
         using var ms = new MemoryStream();
         using var writer = new BinaryWriter(ms);
 
-        var stores = DataSheet.Values.ToList().OrderBy(x => x.StoreID);
+        var stores = DataSheet.Values.OrderBy(x => x.StoreID);
         foreach (var store in stores)
         {
             foreach (var product in store.Products)
