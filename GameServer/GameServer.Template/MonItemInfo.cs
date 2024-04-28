74632c2592b5be2ee11f4995fe41fb005b711297
Damian Day <damianday@users.noreply.github.com>
2024-04-28 20:03:23 +0100

    rename MonsterDrop ->MonItemInfo

--- GameServer/GameServer.Template/MonItemInfo.cs
+++ GameServer/GameServer.Template/MonItemInfo.cs
@@ -1,16 +1,18 @@
 namespace GameServer.Template;
 
-public class MonsterDrop
+public class MonItemInfo
 {
     public string Name;
     public string MonsterName;
     public int 爆率分组;
-    public int Probability;
+    public int SelPoint = 1;
+    public int MaxPoint;
     public int MinAmount;
     public int MaxAmount;
+    public int DropSet;
 
     public override string ToString()
     {
-        return $"{MonsterName} - {Name} - {爆率分组} - {Probability} - {MinAmount}/{MaxAmount}";
+        return $"{MonsterName} - {Name} - {爆率分组} - {SelPoint}/{MaxPoint} - {MinAmount}/{MaxAmount}";
     }
 }
