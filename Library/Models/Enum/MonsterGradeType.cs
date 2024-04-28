1548ca5be75b0a02f7df44baf7c853b9e0198a23
Damian Day <damianday@users.noreply.github.com>
2024-04-28 22:59:26 +0100

    Update MonsterGradeType.cs

--- Library/Models/Enum/MonsterGradeType.cs
+++ Library/Models/Enum/MonsterGradeType.cs
@@ -1,7 +1,13 @@
 ﻿public enum MonsterGradeType
 {
-    Normal = 3,             // 普通怪物
-    Elite = 6,              // 精英干将
-    Boss = 8,               // 头目首领
-    DarkGateMonster = 10    // 暗之门怪物
+    Novelty = 1,            // 小玩意儿
+    Small,                  // 小型怪物
+    Common,                 // 普通怪物
+    Normal = Common,        // 普通怪物
+    Large,                  // 大型怪物
+    Giant,                  // 巨型怪物
+    Elite,                  // 精英干将
+    Rare,                   // 稀有怪物
+    Boss,                   // 头目首领
+    DarkGateMonster = 10,   // 暗之门怪物
 }
