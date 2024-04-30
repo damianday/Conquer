15589bf86531185980d8538d8c4da2c6ab8b9b30
Damian Day <damianday@users.noreply.github.com>
2024-04-30 18:15:07 +0100

    GetBytesReadable

--- Library/Compute.cs
+++ Library/Compute.cs
@@ -1842,6 +1842,29 @@ public static class Compute
         return grid;
     }
 
+    private static string[] sizeSuffixes = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+    public static string GetBytesReadable(long size)
+    {
+        const string formatTemplate = "{0}{1:0.##} {2}";
+
+        if (size == 0)
+        {
+            return string.Format(formatTemplate, null, 0, sizeSuffixes[0]);
+        }
+
+        var absSize = Math.Abs((double)size);
+        var fpPower = Math.Log(absSize, 1_000);
+        var intPower = (int)fpPower;
+        var iUnit = intPower >= sizeSuffixes.Length
+            ? sizeSuffixes.Length - 1
+            : intPower;
+        var normSize = absSize / Math.Pow(1_000, iUnit);
+
+        return string.Format(
+            formatTemplate,
+            size < 0 ? "-" : null, normSize, sizeSuffixes[iUnit]);
+    }
+
     static Compute()
     {
         SystemTime = new DateTime(1970, 1, 1, 8, 0, 0);
