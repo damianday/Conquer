72393f178a41ac17105fbfbb0e7d1d1080b2eb1b
Damian Day <damianday@users.noreply.github.com>
2024-04-30 20:47:03 +0100

    Update ListMonitor.cs

--- GameServer/GameServer.Database/ListMonitor.cs
+++ GameServer/GameServer.Database/ListMonitor.cs
@@ -40,7 +40,7 @@ public sealed class ListMonitor<T> : IEnumerable<T>, IEnumerable
         }
     }
 
-    public IList IList => m_Value;
+    public List<T> IList => m_Value;
     public int Count => m_Value.Count;
     public event ChangedDelegate Changed;
 
