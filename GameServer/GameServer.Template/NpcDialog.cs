a147c3283d1c0869f6518bbfb5d2959c588d9c5a
Damian Day <damianday@users.noreply.github.com>
2024-05-01 23:10:28 +0100

    Fix #14

--- GameServer/GameServer.Template/NpcDialog.cs
+++ GameServer/GameServer.Template/NpcDialog.cs
@@ -1,4 +1,7 @@
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -38,12 +41,76 @@ public sealed class NpcDialog
         DataSheet = new Dictionary<int, string>();
         DataById = new Dictionary<int, byte[]>();
 
-        var path = Settings.Default.GameDataPath + "\\System\\Npc\\Dialogs\\";
-        if (!Directory.Exists(path))
-            return;
+        if (Settings.Default.DBMethod == 0)
+        {
+            var path = Settings.Default.GameDataPath + "\\System\\Npc\\Dialogs\\";
+            if (!Directory.Exists(path))
+                return;
 
-        var array = Serializer.Deserialize<NpcDialog>(path);
-        foreach (var obj in array)
-            DataSheet.Add(obj.ID, obj.Content);
+            var array = Serializer.Deserialize<NpcDialog>(path);
+            foreach (var obj in array)
+                DataSheet.Add(obj.ID, obj.Content);
+        }
+
+        if (Settings.Default.DBMethod == 1)
+        {
+            if (!DBAgent.X.Connected)
+                return;
+
+            try
+            {
+                var qstr = "SELECT * FROM DialogueData";
+                using (var connection = DBAgent.X.DB.GetConnection())
+                {
+                    using var command = DBAgent.X.DB.GetCommand(connection, qstr);
+
+                    using var reader = command.ExecuteReader();
+                    if (reader != null)
+                    {
+                        while (reader.Read() == true)
+                        {
+                            var obj = new NpcDialog
+                            {
+                                ID = reader.GetInt32("ID"),
+                                Content = reader.GetString("Content"),
+                            };
+                            DataSheet.Add(obj.ID, obj.Content);
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                SMain.AddSystemLog(err.ToString());
+            }
+        }
+
+        if (Settings.Default.DBMethod == 2)
+        {
+            var path = Settings.Default.GameDataPath + "\\System";
+            if (!Directory.Exists(path))
+                return;
+
+            using var reader = new StreamReader(path + "\\DialogueData.csv", Encoding.GetEncoding("GB18030"));
+            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csvReader.Read();
+            csvReader.ReadHeader();
+            try
+            {
+                while (csvReader.Read())
+                {
+                    var obj = new NpcDialog
+                    {
+                        ID = csvReader.GetField<int>("ID"),
+                        Content = csvReader.GetField<string>("Content")
+                    };
+                    DataSheet.Add(obj.ID, obj.Content);
+                }
+            }
+            catch (Exception ex)
+            {
+                SEngine.AddSystemLog(ex.Message);
+            }
+        }
     }
 }
