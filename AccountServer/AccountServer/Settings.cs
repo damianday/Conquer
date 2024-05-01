751ca0f935fc9bcae0740b8874bd376435ddd1e3
Damian Day <damianday@users.noreply.github.com>
2024-05-01 23:28:06 +0100

    Unnecessary usings

--- AccountServer/AccountServer/Settings.cs
+++ AccountServer/AccountServer/Settings.cs
@@ -1,9 +1,6 @@
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 
 public class Settings
 {
--- GameServer/GameServer/Settings.cs
+++ GameServer/GameServer/Settings.cs
@@ -1,10 +1,7 @@
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 
 namespace GameServer;
 
