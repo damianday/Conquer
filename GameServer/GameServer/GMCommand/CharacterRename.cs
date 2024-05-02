82d8d7ecd4d958339736b487d6ccead47a26d3c7
Damian Day <damianday@users.noreply.github.com>
2024-05-02 15:49:13 +0100

    Update CharacterRename.cs

--- GameServer/GameServer/GMCommand/CharacterRename.cs
+++ GameServer/GameServer/GMCommand/CharacterRename.cs
@@ -16,33 +16,33 @@ public sealed class CharacterRename : GMCommand
     public override void ExecuteCommand()
     {
         var character = Session.GetCharacter(UserName);
-        if (character != null)
-        {
-            if (character.Connection == null && character.Account.V.Connection == null)
-            {
-                if (Encoding.UTF8.GetBytes(NewUserName).Length > 24)
-                {
-                    SMain.AddCommandLog("<= @" + GetType().Name + " Command failed, role name too long");
-                    return;
-                }
-                if (Session.CharacterInfoTable.SearchTable.ContainsKey(NewUserName))
-                {
-                    SMain.AddCommandLog("<= @" + GetType().Name + " Command failed. Name registered.");
-                    return;
-                }
-                Session.CharacterInfoTable.SearchTable.Remove(character.UserName.V);
-                character.UserName.V = NewUserName;
-                Session.CharacterInfoTable.SearchTable.Add(character.UserName.V, character);
-                SMain.AddCommandLog($"<= @{GetType().Name} Command has been executed, the characters current name: {character}");
-            }
-            else
-            {
-                SMain.AddCommandLog("<= @" + GetType().Name + " Command failed. Account must be taken offline.");
-            }
-        }
-        else
+        if (character == null)
         {
             SMain.AddCommandLog("<= @" + GetType().Name + " Command failed, character does not exist");
+            return;
         }
+
+        if (character.Connected)
+        {
+            SMain.AddCommandLog("<= @" + GetType().Name + " Command failed. Account must be taken offline.");
+            return;
+        }
+
+        if (Encoding.UTF8.GetBytes(NewUserName).Length > 24)
+        {
+            SMain.AddCommandLog("<= @" + GetType().Name + " Command failed, role name too long");
+            return;
+        }
+
+        if (Session.CharacterInfoTable.SearchTable.ContainsKey(NewUserName))
+        {
+            SMain.AddCommandLog("<= @" + GetType().Name + " Command failed. Name registered.");
+            return;
+        }
+
+        Session.CharacterInfoTable.SearchTable.Remove(character.UserName.V);
+        character.UserName.V = NewUserName;
+        Session.CharacterInfoTable.SearchTable.Add(character.UserName.V, character);
+        SMain.AddCommandLog($"<= @{GetType().Name} Command has been executed, the characters current name: {character}");
     }
 }
