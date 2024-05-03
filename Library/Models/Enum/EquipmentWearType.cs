a29a2cd4f1058ce16da5aee01524643b767c8154
Damian Day <damianday@users.noreply.github.com>
2024-05-03 15:27:21 +0100

    EquipmentWearType

---
+++ Library/Models/Enum/EquipmentWearType.cs
@@ -0,0 +1,19 @@
+public enum EquipmentWearType
+{
+    Weapon,         // 武器
+    Armour,         // 衣服
+    Cloak,          // 披风
+    Helmet,         // 头盔
+    ShoulderPad,    // 护肩
+    护腕,
+    Belt,           // 腰带
+    Shoes,          // 鞋子
+    Necklace,       // 项链
+    LeftRing,       // 左戒指
+    RightRing,      // 右戒指
+    LeftBracelet,   // 左手镯
+    RightBracelet,  // 右手镯
+    Medal,          // 勋章
+    玉佩,
+    战具
+}
