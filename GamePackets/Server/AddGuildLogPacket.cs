662216ff15865b4027129f20b842699efcf0ed7b
Damian Day <damianday@users.noreply.github.com>
2024-05-05 18:53:26 +0100

    AddGuildLogPacket

--- GamePackets/Server/AddGuildLogPacket.cs
+++ GamePackets/Server/AddGuildLogPacket.cs
@@ -1,23 +1,23 @@
 namespace GamePackets.Server;
 
 [PacketInfo(Source = PacketSource.Server, ID = 618, Length = 23, Description = "添加公会事记")]
-public sealed class 添加公会事记 : GamePacket
+public sealed class AddGuildLogPacket : GamePacket
 {
     [FieldAttribute(Position = 2, Length = 1)]
-    public byte 事记类型;
+    public byte LogType;
 
     [FieldAttribute(Position = 3, Length = 4)]
-    public int 第一参数;
+    public int Param1;
 
     [FieldAttribute(Position = 7, Length = 4)]
-    public int 第二参数;
+    public int Param2;
 
     [FieldAttribute(Position = 11, Length = 4)]
-    public int 第三参数;
+    public int Param3;
 
     [FieldAttribute(Position = 15, Length = 4)]
-    public int 第四参数;
+    public int Param4;
 
     [FieldAttribute(Position = 19, Length = 4)]
-    public int 事记时间;
+    public int LogTime;
 }
